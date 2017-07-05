04847bbbf696a7d846819dfda1f29e114698369f
SlawekPelka <s.pelka@live.nl>
2017-07-05 10:40:54 +0200

    :pencil: add select option to giphy command

--- App/lib/giphy.js
+++ App/lib/giphy.js
@@ -2,14 +2,26 @@ const grab = require('../data_storage/grab');
 const httpGet = require('../tools/http');
 
 module.exports = {
-    exec(params, message) {
+    exec(params, message, options) {
+        let gif = '';
         let apiURI = `http://api.giphy.com/v1/gifs/search?q=${params}&api_key=${grab.securityTokens('giphy_token')}`;
+        let option = options != '' ? options[0] : '';
 
         function getGif(gifs) {
-            let gif = gifs.data[Math.floor(Math.random() * gifs.data.length)];
-            message.channel.send(gif.url).catch(err => {
-                console.error(err.stack);
-            });
+            try {
+                // Original idea for seledct options by Joe: https://github.com/jddg5wa
+                if (option.includes('select')) {
+                    gif = gifs.data[Number(option.split(':')[1]) - 1];
+                } else {
+                    gif = gifs.data[Math.floor(Math.random() * gifs.data.length)];
+                }
+                
+                message.channel.send(gif.url).catch(err => {
+                    console.error(err.stack);
+                });
+            } catch (e) {
+                message.channel.send('Sorry, I couldn\'t get the requested gif. Please try again');
+            }
         }
 
         httpGet(apiURI, getGif);
@@ -17,7 +29,7 @@ module.exports = {
     metaData() {
         return {
             name: 'giphy',
-            avaliableOptions: '-',
+            avaliableOptions: 'select:<number>',
             description: 'Search for a gif on giphy',
             usage: '<prefix> gif <searchTerm>',
             example: `!c gif cute cats`,
