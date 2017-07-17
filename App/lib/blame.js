6ccc64df41c8fb5ac196092897acfc54e922e024
SlawekPelka <s.pelka@live.nl>
2017-07-17 20:39:54 +0200

    :pencil: wrap blame command in try catch

--- App/lib/blame.js
+++ App/lib/blame.js
@@ -1,13 +1,27 @@
 const grab = require('../data_storage/grab');
+const logger = require('../tools/dataLogger');
 const moment = require('moment');
 
 module.exports = {
     exec(params, message, options, client) {
         if (!params.match(/[0-9]+/g)) return message.channel.send("Issued params are not an ID!");
-        let blameList = grab.logs('blame', message.channel.id, '', params);
-        let user = client.users.get(Object.keys(blameList)[0]);
+        try {
+            let blameList = grab.logs('blame', message.channel.id, '', params);
+            let user = client.users.get(Object.keys(blameList)[0]);
 
-        message.channel.send(`***Blame ${params}!***\nCommand issued by: **${user.username}#${user.discriminator}**\nOn: **${moment(blameList[0]).format('LLLL')}**`);
+            message.channel.send(`***Blame ${params}!***\nCommand issued by: **${user.username}#${user.discriminator}**\nOn: **${moment(blameList[0]).format('LLLL')}**`)
+                .then(m => {
+                    dataLog.resolveOveralUsage(
+                        m.guild.id,
+                        message.author.id,
+                        m.id,
+                        module.exports.metaData().name
+                    );
+                });
+        } catch (e) {
+            console.error(e.stack);
+            message.channel.send("There was a problem with requested blame!\n Issued blame ID most probably doesn't exist.");
+        }
     },
     metaData() {
         return {
