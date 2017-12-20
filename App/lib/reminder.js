d7e8dd6eca22c1760795657e397780c226e028b7
SlawekPelka <s.pelka@live.nl>
2017-12-20 15:04:18 +0100

    :newspaper: brand new remind command

---
+++ App/lib/reminder.js
@@ -0,0 +1,23 @@
+const humanizeDuration = require('humanize-duration')
+
+module.exports = {
+    exec(params, message, options, client) {
+        var time = parseInt(options[0]) ? parseInt(options[0]) : 1000;
+        setTimeout(() => {
+            message.author.send(params).then(m => {
+                message.channel.send(`Will remind you of **${params}** in *${humanizeDuration(time)}*`);
+            });
+        }, time);
+    },
+    metaData() {
+        return {
+            name: 'Reminder',
+            avaliableOptions: '-',
+            description: 'Set a reminder for certain time',
+            usage: '<prefix> remind <thing> <timestamp>',
+            example: `!c remind something 1000`,
+            group: 'utility',
+            execWith: 'remind'
+        }
+    }
+}
